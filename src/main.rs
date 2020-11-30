eff0c8845bd04e9ccf5e94921c95d38a2cdc1089
Evgeny Papazyan <swordelf@mail.ru>
2020-11-30 19:14:58 +0300

    Chapter 2.4 of Bevy Book finished

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,37 @@
 use bevy::prelude::*;
 
-/* Bevy uses ECS */
+/* Bevy uses ECS --- Entity Component System */
+
+// newtype pattern
+struct Name(String);
+
+// Our first component --- unit struct
+struct Person;
+
+// Our first startup system
+fn add_people(mut commands: Commands) {
+    commands
+        .spawn((Person {}, Name("Elaina Proctor".to_string())))
+        .spawn((Person {}, Name("Renzo Hume".to_string())))
+        .spawn((Person {}, Name("Zayna Nieves".to_string())));
+}
 
 // Our first system
 fn hello_world() {
     println!("Hello, world!");
 }
 
+// Our first normal system
+fn greet_people(_person: &Person, name: &Name) {
+    println!("hello, {}!", name.0);
+}
+
 fn main() {
     App::build()
+        .add_startup_system(add_people.system())
         .add_system(hello_world.system())
-        .run();
+        .add_system(greet_people.system())
+        .run()
 
     // ? How is it possible to call a function on a function name as in `hello_world.system()`?
 }
