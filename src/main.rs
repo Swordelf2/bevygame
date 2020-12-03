a51d5936a4c84c994bd048e1a96ef6a9bd8b78d5
Egor Zinkin <pifagor36@yandex.ru>
2020-12-03 23:08:06 +0300

    Rotate movement

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 
+
 // Marker component
 struct Player;
 
@@ -49,22 +50,38 @@ fn player_control(
     _player: &Player,
     mut transform: Mut<Transform>,
 ) {
-    const SPEED: f32 = 300.0;
-    if input.pressed(KeyCode::A) {
-        transform.translation += Vec3::new(-time.delta_seconds * SPEED, 0.0, 0.0);
-    }
-    if input.pressed(KeyCode::D) {
-        transform.translation += Vec3::new(time.delta_seconds * SPEED, 0.0, 0.0);
-    }
-    if input.pressed(KeyCode::W) {
-        transform.translation += Vec3::new(0.0, time.delta_seconds * SPEED, 0.0);
-    }
-    if input.pressed(KeyCode::S) {
-        transform.translation += Vec3::new(0.0, -time.delta_seconds * SPEED, 0.0);
-    }
 
     const ROTATION_SPEED: f32 = 1.0;
-    if input.pressed(KeyCode::Z) {
-        transform.rotation = Quat::from_rotation_z(transform.rotation.to_axis_angle().1 + time.delta_seconds * ROTATION_SPEED);
+    const SPEED: f32 = 300.0;
+
+    let angle = transform.rotation.to_axis_angle().1 + time.delta_seconds * ROTATION_SPEED;
+    let angle = if angle >= 2.0 * PI 
+        { angle - 2.0 * PI  } 
+        else { angle };
+
+    transform.rotation = Quat::from_rotation_z(angle);
+
+    
+    if input.pressed(KeyCode::A) {
+        transform.translation += Vec3::new(-time.delta_seconds * SPEED * angle.cos(), -time.delta_seconds * SPEED * angle.sin(), 0.0);
     }
+    if input.pressed(KeyCode::D) {
+        transform.translation += Vec3::new(time.delta_seconds * SPEED * angle.cos(), time.delta_seconds * SPEED * angle.sin(), 0.0);
+    }
+    if input.pressed(KeyCode::W) {
+        transform.translation += Vec3::new(-time.delta_seconds * SPEED * angle.sin(), time.delta_seconds * SPEED * angle.cos(), 0.0);
+    }
+    if input.pressed(KeyCode::S) {
+        transform.translation += Vec3::new(time.delta_seconds * SPEED * angle.sin(), -time.delta_seconds * SPEED * angle.cos(), 0.0);
+    }
+
+
+
+
+
+    /*
+    let rotor = Quat::from_rotation_z(ROTATION_SPEED * time.delta_seconds);
+    transform.rotate(rotor);
+    */
+
 }
