b95669309553211274fdfaa139a031a38949144d
Evgeny Papazyan <swordelf@mail.ru>
2021-04-12 18:50:32 +0300

    Version 0.2 finished

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
 use bevy::prelude::*;
 use core::f32::consts::PI;
 
+use bevy::render::camera::Camera;
 use bevy::sprite::collide_aabb::collide;
+
+// Cell size in pixels
+const CELL_SIZE: f32 = 32.0;
+const MOVE_SPEED: f32 = CELL_SIZE * 3.0;
+const ROTATION_SPEED: f32 = 1.0;
 
 #[derive(Default)]
 struct Player {
-    has_collided: bool,
+    has_collided: Option<Collider>,
 }
 
-struct Hazard;
+#[derive(Clone, Copy)]
+enum Collider {
+    Hazard,
+    Princess,
+}
+
+// StartPos resource
+struct StartPos {
+    x: f32,
+    y: f32,
+}
 
 fn main() {
     App::build()
@@ -28,6 +44,8 @@ fn main() {
         // Systems
         .add_system(player_control.system())
         .add_system(collision_system.system())
+        .add_system(score_system.system())
+        .add_system(camera_movement.system())
         .run();
 }
 
@@ -36,46 +54,86 @@ fn setup(
     asset_server: Res<AssetServer>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    // Load player texture
-    let player_texture_handle: Handle<_> = asset_server.load("player.png");
+    // Load player and princess textures
+    let player_material_handle = materials.add(asset_server.load("player.png").into());
+    let princess_material_handle = materials.add(asset_server.load("princess.png").into());
 
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    // player
-    commands
-        .spawn_bundle(SpriteBundle {
-            material: materials.add(player_texture_handle.into()),
-            transform: Transform::from_translation(Vec3::new(0.0, -215.0, 0.0)),
-            sprite: Sprite::new(Vec2::new(120.0, 120.0)),
-            ..Default::default()
-        })
-        .insert(Player::default());
+    // Load map image
+    let map_img = image::open("assets/map.png")
+        .expect("File map.png not found")
+        .into_rgb8();
 
-    // Hazard squares
-    commands
-        .spawn_bundle(SpriteBundle {
-            material: materials.add(Color::rgb(0.0, 0.0, 0.9).into()),
-            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
-            sprite: Sprite::new(Vec2::new(120.0, 120.0)),
-            ..Default::default()
-        })
-        .insert(Hazard);
+    // Iterate over the pixels of the image and spawn corresponding cells (player and hazards)
+    for (x, mut y, &pixel) in map_img.enumerate_pixels() {
+        // Invert the y axis
+        y = map_img.height() - y - 1;
+        // Calculate cell position
+        let cell_pos_x = x as f32 * CELL_SIZE + CELL_SIZE / 2.0;
+        let cell_pos_y = y as f32 * CELL_SIZE + CELL_SIZE / 2.0;
+
+        match pixel.0 {
+            // Red = Princess (finish cell)
+            [255, 0, 0] => {
+                // Spawn princess
+                commands
+                    .spawn_bundle(SpriteBundle {
+                        material: princess_material_handle.clone(),
+                        transform: Transform::from_translation(Vec3::new(
+                            cell_pos_x, cell_pos_y, 0.1,
+                        )),
+                        sprite: Sprite::new(Vec2::new(CELL_SIZE, CELL_SIZE)),
+                        ..Default::default()
+                    })
+                    .insert(Collider::Princess);
+            }
+            // Green = Start cell
+            [0, 255, 0] => {
+                // Spawn player
+                commands
+                    .spawn_bundle(SpriteBundle {
+                        material: player_material_handle.clone(),
+                        transform: Transform::from_translation(Vec3::new(
+                            cell_pos_x, cell_pos_y, 0.2,
+                        )),
+                        sprite: Sprite::new(Vec2::new(CELL_SIZE, CELL_SIZE)),
+                        ..Default::default()
+                    })
+                    .insert(Player::default());
+                // Store the start position as a resource
+                commands.insert_resource(StartPos {
+                    x: cell_pos_x,
+                    y: cell_pos_y,
+                });
+            }
+            // Blue = Hazard cell
+            [0, 0, 255] => {
+                // Spawn a hazard cell
+                commands
+                    .spawn_bundle(SpriteBundle {
+                        material: materials.add(Color::rgb(0.0, 0.0, 0.9).into()),
+                        transform: Transform::from_translation(Vec3::new(
+                            cell_pos_x, cell_pos_y, 0.1,
+                        )),
+                        sprite: Sprite::new(Vec2::new(CELL_SIZE, CELL_SIZE)),
+                        ..Default::default()
+                    })
+                    .insert(Collider::Hazard);
+            }
+            _ => {}
+        }
+    }
+
+    // Spawn camera
+    let camera = OrthographicCameraBundle::new_2d();
+    commands.spawn_bundle(camera);
 }
 
 fn player_control(
     input: Res<Input<KeyCode>>,
     time: Res<Time>,
-    mut query: Query<(&mut Transform, &mut Player)>,
+    mut query: Query<&mut Transform, With<Player>>,
 ) {
-    let (mut transform, mut player) = query.single_mut().expect("No player entity found");
-
-    // if has collided, reset position
-    if player.has_collided {
-        transform.translation = Vec3::new(0.0, -215.0, 0.0);
-        player.has_collided = false;
-    }
-
-    const ROTATION_SPEED: f32 = 1.0;
-    const SPEED: f32 = 300.0;
+    let mut transform = query.single_mut().expect("No player entity found");
 
     let angle = transform.rotation.to_axis_angle().1 + time.delta_seconds() * ROTATION_SPEED;
     let angle = if angle >= 2.0 * PI {
@@ -88,29 +146,29 @@ fn player_control(
 
     if input.pressed(KeyCode::A) {
         transform.translation += Vec3::new(
-            -time.delta_seconds() * SPEED * angle.cos(),
-            -time.delta_seconds() * SPEED * angle.sin(),
+            -time.delta_seconds() * MOVE_SPEED * angle.cos(),
+            -time.delta_seconds() * MOVE_SPEED * angle.sin(),
             0.0,
         );
     }
     if input.pressed(KeyCode::D) {
         transform.translation += Vec3::new(
-            time.delta_seconds() * SPEED * angle.cos(),
-            time.delta_seconds() * SPEED * angle.sin(),
+            time.delta_seconds() * MOVE_SPEED * angle.cos(),
+            time.delta_seconds() * MOVE_SPEED * angle.sin(),
             0.0,
         );
     }
     if input.pressed(KeyCode::W) {
         transform.translation += Vec3::new(
-            -time.delta_seconds() * SPEED * angle.sin(),
-            time.delta_seconds() * SPEED * angle.cos(),
+            -time.delta_seconds() * MOVE_SPEED * angle.sin(),
+            time.delta_seconds() * MOVE_SPEED * angle.cos(),
             0.0,
         );
     }
     if input.pressed(KeyCode::S) {
         transform.translation += Vec3::new(
-            time.delta_seconds() * SPEED * angle.sin(),
-            -time.delta_seconds() * SPEED * angle.cos(),
+            time.delta_seconds() * MOVE_SPEED * angle.sin(),
+            -time.delta_seconds() * MOVE_SPEED * angle.cos(),
             0.0,
         );
     }
@@ -121,24 +179,58 @@ fn player_control(
     */
 }
 
+fn camera_movement(
+    mut q: QuerySet<(
+        Query<&mut Transform, With<Camera>>,
+        Query<&Transform, With<Player>>,
+    )>,
+) {
+    // Retrieve player's position and size
+    let player_transform = q.q1().single().expect("Player entity not found");
+    let player_pos = *player_transform.translation;
+
+    let mut camera_transform = q.q0_mut().single_mut().expect("Camera entity not found");
+    camera_transform.translation.x = player_pos.x;
+    camera_transform.translation.y = player_pos.y;
+}
+
+fn score_system(start_pos: Res<StartPos>, mut player_query: Query<(&mut Transform, &mut Player)>) {
+    let (mut transform, mut player) = player_query.single_mut().expect("Player entity not found");
+    // if has collided, reset position
+    match player.has_collided {
+        Some(Collider::Hazard) => {
+            transform.translation.x = start_pos.x;
+            transform.translation.y = start_pos.y;
+            player.has_collided = None;
+        }
+        Some(Collider::Princess) => {
+            println!("YOU WON");
+            transform.translation.x = start_pos.x;
+            transform.translation.y = start_pos.y;
+            player.has_collided = None;
+        }
+        None => {}
+    }
+}
+
 fn collision_system(
     mut player_query: Query<(&Transform, &Sprite, &mut Player)>,
-    hazard_query: Query<(&Transform, &Sprite), With<Hazard>>,
+    collider_query: Query<(&Transform, &Sprite, &Collider)>,
 ) {
     let (player_transform, player_sprite, mut player) =
         player_query.single_mut().expect("No player entity found");
 
     // Iterate over all hazards
-    for (hazard_transform, hazard_sprite) in hazard_query.iter() {
+    for (collider_transform, collider_sprite, collider) in collider_query.iter() {
         if collide(
             player_transform.translation,
             player_sprite.size,
-            hazard_transform.translation,
-            hazard_sprite.size,
+            collider_transform.translation,
+            collider_sprite.size,
         )
         .is_some()
         {
-            player.has_collided = true;
+            player.has_collided = Some(*collider);
             println!("Collided!");
         }
     }
